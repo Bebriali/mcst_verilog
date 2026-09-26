87d1a550abe466398548447784e975089d93990d
Bebriali <ostashkinm@inbox.ru>
2026-09-26 16:01:07 +0300

    modify task5 to a bigger entry

--- task5/mod3.v
+++ task5/mod3.v
@@ -1,13 +1,36 @@
 module mod3
 (
-    input [7:0] in,
+    input [15:0] in,
     output is_div_3
 );
-    wire [2:0] sum_even = in[0] + in[2] + in[4] + in[6];
-    wire [2:0] sum_odd  = in[1] + in[3] + in[5] + in[7];
-    
-    wire [1:0] rem_even = (sum_even >= 3) ? (sum_even - 3) : sum_even;
-    wire [1:0] rem_odd  = (sum_odd >= 3) ? (sum_odd - 3) : sum_odd;
-    
-    assign is_div_3 = (rem_even == rem_odd);
+    wire [3:0] sum_even = {3'b000, in[0]} + {3'b000, in[2]} +
+                          {3'b000, in[4]} + {3'b000, in[6]} +
+                          {3'b000, in[8]} + {3'b000, in[10]} +
+                          {3'b000, in[12]} + {3'b000, in[14]};
+    wire [3:0] sum_odd  = {3'b000, in[1]} + {3'b000, in[3]} +
+                          {3'b000, in[5]} + {3'b000, in[7]} +
+                          {3'b000, in[9]} + {3'b000, in[11]} +
+                          {3'b000, in[13]} + {3'b000, in[15]};
+    wire  div_even;
+    wire  div_odd;
+
+    xmod3 is_div_even(.xin(sum_even), .xis_div_3(div_even));
+    xmod3 is_div_odd (.xin(sum_odd),  .xis_div_3(div_odd));
+
+    assign is_div_3 = (div_even == div_odd);
 endmodule
+
+
+// добавка 'x' к названию модуля и переменных как extra
+// не уверен, что можно как то доработать нейминг
+module xmod3
+(
+    input [3:0] xin,
+    output xis_div_3
+);
+    wire [1:0] sum_even = xin[0] + xin[2];
+    wire [1:0] sum_odd  = xin[1] + xin[3];
+
+    assign xis_div_3 = (sum_even == sum_odd);
+endmodule
+
--- task5/top.v
+++ task5/top.v
@@ -1,13 +1,13 @@
 `timescale 1ns/100ps
 
 module top();
-reg     [7:0] val;
+reg     [15:0] val;
 wire     res;
 
 mod3 module_of_three(
     .in(val),
     .is_div_3(res)
-); 
+);
 initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, top);
@@ -16,6 +16,11 @@ initial begin
     val = 8'b00000100; #10;
     val = 8'b00001100; #10;
     val = 8'b00000011; #10;
+    val = 16'hffff;    #10;
+    val = 16'hdabc;    #10;
+    val = 16'hfbac;    #10;
+    val = 16'd80;      #10;
+    val = 16'd81;      #10;
 
     $finish;
 end
