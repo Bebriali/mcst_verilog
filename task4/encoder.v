8f89435bfec33975c42a76b2b0c34f3b5fb5680b
maxim <ostashkinm@inbox.ru>
2026-09-26 15:53:53 +0300

    modify task4

--- task4/encoder.v
+++ task4/encoder.v
@@ -1,18 +1,18 @@
 module \encoder3->8 (
     input   wire    [7:0] in,
-    output  reg     [2:0] out
+    output  wire    [2:0] out,
+    output  wire    valid
 );
 
-    integer k;
+    assign out = in[7] ? 3'd7 :
+                 in[6] ? 3'd6 :
+                 in[5] ? 3'd5 :
+                 in[4] ? 3'd4 :
+                 in[3] ? 3'd3 :
+                 in[2] ? 3'd2 :
+                 in[1] ? 3'd1 : 
+                 3'd0;
 
-    always @(*) begin
-        out = 3'd0;
-        
-        for (k = 0; k < 8; k = k + 1) begin
-            if (in[k]) begin
-                out = k[2:0];
-            end
-        end
-    end
+    assign valid = |in;
 
 endmodule
